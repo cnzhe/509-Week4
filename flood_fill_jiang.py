4d98f67786bc27ecb464c6c7293dbd0dcab6bf86
jiaqiwu66 <you@example.com>
2023-10-21 17:37:39 -0700

    add test board_2, add docstrings, add 2 assert statements

--- flood_fill_jiang.py
+++ flood_fill_jiang.py
@@ -11,6 +11,18 @@ board = [
     "....##############....",
 ]
 
+board_2 = [
+    "......................",
+    "......##########......",
+    "......#....#...#......",
+    "......#....#...#......",
+    "......#....#...#####..",
+    "....###....#.......#..",
+    "....#......#.....###..",
+    "....##############....",
+]
+
+
 def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
     """Returns board with old values replaced with new values
     through flood filling starting from the coordinates x, y
@@ -23,11 +35,14 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     Returns:
         List[str]: Modified board
     """
+    # when input out of the board
     if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]):
         return input_board
 
     if input_board[x][y] == old:
+        # replace old with new through string concatenation
         input_board[x] = input_board[x][:y] + new + input_board[x][y + 1:]
+        # in four directions
         flood_fill(input_board, old, new, x + 1, y)
         flood_fill(input_board, old, new, x - 1, y)
         flood_fill(input_board, old, new, x, y + 1)
@@ -37,6 +52,9 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
 
 
 modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+modified_board_2 = flood_fill(input_board=board_2, old=".", new="~", x=5, y=12)
+
+print('test case1')
 
 for a in modified_board:
     print(a)
@@ -51,3 +69,42 @@ for a in modified_board:
 # ....###~~~~~~~~~~~~#..
 # ....#~~~~~~~~~~~~###..
 # ....##############....
+
+print('test case2')
+for a in modified_board_2:
+    print(a)
+
+# Expected output:
+# ......................
+# ......##########......
+# ......#....#~~~#......
+# ......#....#~~~#......
+# ......#....#~~~#####..
+# ....###....#~~~~~~~#..
+# ....#......#~~~~~###..
+# ....##############....
+
+expected_output = [
+    '......................',
+    '......##########......',
+    '......#~~~~~~~~#......',
+    '......#~~~~~~~~#......',
+    '......#~~~~~~~~#####..',
+    '....###~~~~~~~~~~~~#..',
+    '....#~~~~~~~~~~~~###..',
+    '....##############....'
+]
+
+expected_output_2 = [
+    '......................',
+    '......##########......',
+    '......#....#~~~#......',
+    '......#....#~~~#......',
+    '......#....#~~~#####..',
+    '....###....#~~~~~~~#..',
+    '....#......#~~~~~###..',
+    '....##############....'
+]
+
+assert modified_board == expected_output
+assert modified_board_2 == expected_output_2
